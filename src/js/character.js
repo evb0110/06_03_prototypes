0fa48b9907eb7f002fc7786afb1cd671922a4a9d
evb0110 <7149553@gmail.com>
2019-02-03 23:56:31 +0300

    solved

--- src/js/character.js
+++ src/js/character.js
@@ -1,14 +1,14 @@
 /**
  * <Общее описание>
- * 
+ *
  * @param {string} name - character's name
  * @param {string} type - character's type
- * 
+ *
  * @throws Will throw an error if name.length < 2
  * @throws Will throw an error if name.length > 10
  * @throws Will throw an error if name isn't a string
  * @throws Will throw if type doesn't belong to a predefined array of types
- */ 
+ */
 function Character(name, type) {
   this.name = name;
   this.type = type;
@@ -20,7 +20,7 @@ function Character(name, type) {
 Character.prototype.damage = function damage(points) {
   const healthDamage = points * (1 - this.defence / 100);
   this.health -= healthDamage;
-}
+};
 
 
 export default Character;
