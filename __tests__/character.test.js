829647c1b8538db403ef63ef245b13c97ffed505
evb0110 <7149553@gmail.com>
2019-02-07 17:19:52 +0300

    fixed imports

---
+++ __tests__/character.test.js
@@ -0,0 +1,18 @@
+import Character from '../src/js/character';
+
+test('testing with a simple character', () => {
+  const input = ['John', 'Bowman'];
+  const john = new Character(...input);
+
+  john.damage(10);
+
+  const expected = {
+    name: 'John',
+    type: 'Bowman',
+    health: 94,
+    attack: 10,
+    defence: 40,
+  };
+
+  expect(john).toEqual(expected);
+});
